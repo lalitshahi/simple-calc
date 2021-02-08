f89750bea068e3ab01379154fd23a5f5dd5078b1
Lalit Singh Shahi <shahilalit002@gmail.com>
2021-02-08 18:47:49 +0530

    goal 2 complete

--- src/components/calc_button_row.js
+++ src/components/calc_button_row.js
@@ -1,18 +1,26 @@
 import React from "react";
 import CalcButton from "components/calc_button";
-import { map } from "lodash";
+import { filter, map } from "lodash";
 
-const CalcButtonRow = ({ numArray, handleClick }) => {
-  const renderRows = () =>
-    map(numArray, (num) => (
-      <CalcButton
-        value={num.value}
-        number={num.number}
-        key={num.value}
-        handleClick={handleClick}
-      />
-    ));
+const CalcButtonRow = ({ numArray, handleClick, isScientificMode }) => {
+  const renderRows = () => {
+    let numPadsToShow = numArray;
 
+    if (!isScientificMode) {
+      numPadsToShow = filter(numArray, (num) => !num.scientificMode);
+    }
+
+    return map(numPadsToShow, (num) => {
+      return (
+        <CalcButton
+          value={num.value}
+          number={num.number}
+          key={num.value}
+          handleClick={handleClick}
+        />
+      );
+    });
+  };
   return <div className="calc_button_row">{renderRows()}</div>;
 };
 
