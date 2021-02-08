88d53fef8d118bbd8048a797e4a6be4890d0d59e
Lalit Singh Shahi <shahilalit002@gmail.com>
2021-02-08 13:38:07 +0530

    jsconfig for absolute path and calc container

---
+++ src/containers/calc.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CalculatorContainer extends Component {
+  render() {
+    return (
+      <div className="calc_container">
+        <p>Calcu</p>
+      </div>
+    );
+  }
+}
+
+export default CalculatorContainer;
